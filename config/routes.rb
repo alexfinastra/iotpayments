d854e36f9adaf6d6db74206a70860c7f44eacc31
alexperman <alex.perman@gmail.com>
2018-02-21 05:39:01 +0200

    hackathon #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :contracts
-  resources :merchants
-  resources :users
-  resources :devices
-  resources :payments
-  resources :accounts
+  #resources :contracts
+  #resources :merchants
+  #resources :users
+  #resources :devices
+  #resources :payments
+  #resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root :to => "payments#index"
   
 end
